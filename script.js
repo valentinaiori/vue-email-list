777ff1f94584da914e5dfae73bd9baf8b506aaca
valentinaiori <valentinaiori@live.it>
2022-09-20 16:12:06 +0200

    Add complete exercise

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('Ok!')
+console.log('JS OK!')
+
+
+
+const app = new Vue({
+    el: '#app', 
+    data: {
+        emails:[],
+    },
+
+    methods:{
+        generateEmails(){
+                for (let i = 0; i < 10; i++){
+                    axios
+                        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                        .then((response) => {
+    
+                            const randomEmail = response.data.response;
+                            console.log(randomEmail)
+
+                            this.emails.push(randomEmail)                            
+                });
+            }
+        },
+
+    },
+
+    mounted(){
+        this.generateEmails()
+
+    }
+});
+
